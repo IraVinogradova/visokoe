5df38d422add6bb7d33e4b188fd6192d487d8ccb
Ira Vinogradova <genna324@gmail.com>
2023-08-18 15:33:13 +0300

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace visokoe
 {
-    partial class Form1
+    partial class Panel
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,21 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.serverison = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.PLCcon = new System.Windows.Forms.Button();
+            this.fromPLC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ser_on = new System.Windows.Forms.Label();
+            this.ser_cl = new System.Windows.Forms.Label();
+            this.text_test = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(118, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 96);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "PLC connect";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // serverison
             // 
@@ -50,31 +44,95 @@
             this.serverison.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.serverison.Location = new System.Drawing.Point(12, 9);
             this.serverison.Name = "serverison";
-            this.serverison.Size = new System.Drawing.Size(519, 29);
+            this.serverison.Size = new System.Drawing.Size(237, 29);
             this.serverison.TabIndex = 4;
-            this.serverison.Text = "Сервер запущен.Ожидание подключения...";
+            this.serverison.Text = "Клиент подключен";
             this.serverison.Visible = false;
-            this.serverison.Click += new System.EventHandler(this.serverison_Click);
+            
             // 
-            // button3
+            // PLCcon
             // 
-            this.button3.Location = new System.Drawing.Point(118, 234);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 91);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "PLC connect2";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.PLCcon.Location = new System.Drawing.Point(34, 77);
+            this.PLCcon.Name = "PLCcon";
+            this.PLCcon.Size = new System.Drawing.Size(84, 42);
+            this.PLCcon.TabIndex = 6;
+            this.PLCcon.Text = "СТАРТ";
+            this.PLCcon.UseVisualStyleBackColor = true;
+            this.PLCcon.Click += new System.EventHandler(this.PLCcon_Click);
             // 
-            // Form1
+            // fromPLC
+            // 
+            this.fromPLC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fromPLC.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.fromPLC.Location = new System.Drawing.Point(478, 253);
+            this.fromPLC.Name = "fromPLC";
+            this.fromPLC.Size = new System.Drawing.Size(461, 38);
+            this.fromPLC.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "fromPLC";
+            // 
+            // ser_on
+            // 
+            this.ser_on.AutoSize = true;
+            this.ser_on.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ser_on.Location = new System.Drawing.Point(763, 9);
+            this.ser_on.Name = "ser_on";
+            this.ser_on.Size = new System.Drawing.Size(149, 29);
+            this.ser_on.TabIndex = 10;
+            this.ser_on.Text = "server is on";
+            this.ser_on.Visible = false;
+        
+            // 
+            // ser_cl
+            // 
+            this.ser_cl.AutoSize = true;
+            this.ser_cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ser_cl.Location = new System.Drawing.Point(763, 38);
+            this.ser_cl.Name = "ser_cl";
+            this.ser_cl.Size = new System.Drawing.Size(183, 29);
+            this.ser_cl.TabIndex = 11;
+            this.ser_cl.Text = "server is close";
+            // 
+            // text_test
+            // 
+            this.text_test.AutoSize = true;
+            this.text_test.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text_test.Location = new System.Drawing.Point(499, 321);
+            this.text_test.Name = "text_test";
+            this.text_test.Size = new System.Drawing.Size(27, 29);
+            this.text_test.TabIndex = 12;
+            this.text_test.Text = "#";
+            this.text_test.UseMnemonic = false;
+            // 
+            // Panel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(970, 558);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.text_test);
+            this.Controls.Add(this.ser_cl);
+            this.Controls.Add(this.ser_on);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fromPLC);
+            this.Controls.Add(this.PLCcon);
             this.Controls.Add(this.serverison);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "Panel";
             this.Text = "Panel";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -82,10 +140,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label serverison;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button PLCcon;
+        private System.Windows.Forms.TextBox fromPLC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ser_on;
+        private System.Windows.Forms.Label ser_cl;
+        private System.Windows.Forms.Label text_test;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,90 +11,103 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.Net.Http;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
 
 namespace visokoe
-{
-    public partial class Form1 : Form
-        
-
-
-
+{  
+    public partial class Panel : Form
     {
-        
-
-        public Form1()
+       
+        public async  void Panel1()
         {
-
-            InitializeComponent();
-
-        }
-
-        private void serverison_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private async void button1_Click(object sender, EventArgs e)
-        {
-            // Настраиваем обмен
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);  // Сервер запущен на 127.0.0.1 порт 8888 !
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Bind(ipPoint);
-            // включаем метод listen
-            socket.Listen(1000);
-           
             
-           
-
-            while (true)
-            {
-
-                // получаем подключение клиента
-                Socket client = await socket.AcceptAsync();
-                serverison.Visible = true;  // Есть подключение - видна надпись, что можно есть коннект 
-                if (client.Connected)
-                    serverison.Enabled = false; // чтобы не нажать кнопку еще раз
-                byte[] responseData = new byte[512];
-                // bytes = 0;
-                int bytes = 0;
-
-                var response = new StringBuilder(); // для склеивания данных 
-
-                //SocketAsyncEventArgs readArg = SocketAsyncEventArgs();
-                /*do
-                {
-                    bytes = await socket.ReceiveAsync(SocketAsyncEventArgs); 
-                    response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
-                }
-                while (bytes > 0); */
-            }
-
-
-            ///
-           
-
-            ///
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
+            InitializeComponent();
             // Запускаем сервер на адресе 127.0.0.1 и порту 8888
             IPAddress localAdr = IPAddress.Parse("127.0.0.1");
             TcpListener server = new TcpListener(localAdr, 8888);
 
             IPEndPoint ipLocalEndPoint = new IPEndPoint(localAdr, 8888);
+            try
+            {
+                server.Start(); ser_cl.Visible = false;
+                ser_on.Visible = true;
+           
+            ;
             // Запуск сервера
             server.Start();
+            TcpClient client = await server.AcceptTcpClientAsync();
+            byte[] receivedBuffer = new byte[1024];
+            string msg = "";
+            // if (msg.Length !=0) { 
+            // msg.Remove(msg.Length - 1);
+            // }
+            NetworkStream stream = client.GetStream();
+            string end_bute = "\n";
+            byte[] end_buff = System.Text.Encoding.Default.GetBytes(end_bute);
+            if (client.Connected ) { serverison.Visible = true; };
 
+                // читаем данные
+                do
+                {
+
+                    // if (receivedBuffer.Take(3).Equals(end_buff))
+                    //{
+                    //    fromPLC.Clear(); break;
+                    // }
+
+                    //stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+                    var bytesread = await stream.ReadAsync(receivedBuffer, 0, receivedBuffer.Length);
+                    msg = Encoding.UTF8.GetString(receivedBuffer, 0, bytesread);
+                    fromPLC.Text = msg;
+                    text_test.Text = msg;
+
+                }
+                while (client.Connected);
+
+            }
+            catch (Exception ex)
+            {
+                server.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+ 
+
+        private async void PLCcon_Click(object sender, EventArgs e)
+        {
+               /* do
+                {
+
+                    // if (receivedBuffer.Take(3).Equals(end_buff))
+                    //{
+                    //    fromPLC.Clear(); break;
+                    // }
+
+                    //stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+                    var bytesread = await stream.ReadAsync(receivedBuffer, 0, receivedBuffer.Length);
+                    msg = Encoding.UTF8.GetString(receivedBuffer, 0, bytesread);
+                    fromPLC.Text = msg;
+                    text_test.Text = msg;
+
+                }
+                while (client.Connected);
+
+            }
+                catch (Exception ex)
+                {
+                    server.Stop();
+                    MessageBox.Show(ex.Message);
+                }
+    //TcpClient client = await server.AcceptTcpClientAsync(); */
+}
+
+        private void label3_Click(object sender, EventArgs e)
+        {
 
         }
     }
-        
-    
+            
 }
